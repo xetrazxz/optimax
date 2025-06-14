28b3e6bbbd3527df48eaaf9cb07a138bb8f4cc5d
xetrazxz <xetrazx@gmail.com>
2025-06-14 19:25:17 +0530

    Last Sync: 2025-06-14 19:25 (Mobile)

--- config/sky-uc.sh
+++ config/sky-uc.sh
@@ -5,9 +5,13 @@ CODENAME="sky"
 echo $DEVICENAME
 echo $CODENAME
 #
-echo "Underclocker"
+echo "Underclocker....."
+echo "Setting PowerSave Governer ......."
+sleep 1
+echo "Setting Lower Clock Speed"
 #
 su -c "echo "powersave" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
 su -c "echo "powersave" > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor"
 su -c "echo "1632000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq"
 su -c "echo "1900800" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq"
+exit 0

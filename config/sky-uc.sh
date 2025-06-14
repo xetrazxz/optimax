d0c46ef08bbf3e16f474b132dfa49b2c8aac46dc
xetrazxz <xetrazx@gmail.com>
2025-06-14 19:17:56 +0530

    Last Sync: 2025-06-14 19:17 (Mobile)

--- config/sky-uc.sh
+++ config/sky-uc.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#
 DEVICENAME="Redmi 12 5G / POCO M6 Pro 5G"
+CODENAME="sky"
 #
-echo "Underclock"
 echo $DEVICENAME
+echo $CODENAME
+#
+echo "Underclocker"
 #
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy6/scaling_available_governors

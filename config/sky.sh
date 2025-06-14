504038049efcc45ba1691a076a7f6105b8529a21
xetrazxz <xetrazx@gmail.com>
2025-06-14 18:48:45 +0530

    Last Sync: 2025-06-14 18:48 (Mobile)

--- config/sky.sh
+++ config/sky.sh
@@ -1,1 +1,67 @@
-echo "Device : REDMI 12 5G / POCO M6 Pro 5G / REDMI NOTE 12R"
+#!/bin/sh
+#
+DEVICENAME="Redmi 12 5G / POCO M6 Pro 5G"
+CODENAME="sky"
+#
+echo $DEVICENAME
+echo $CODENAME
+#
+echo ""
+echo "Setting I/O scheduler"
+for queue in /sys/block/sd*/queue
+do
+    echo "none" > "$queue/scheduler"
+    echo "0" > "$queue/iostats"
+    echo "0" > "$queue/add_random"
+    echo "0" > "$queue/nomerges"
+    echo "128" > "$queue/nr_requests"
+    echo "2" > "$queue/rq_affinity"
+    echo "1024" > "$queue/read_ahead_kb"
+done ;
+
+for queue in /sys/block/loop*/queue
+do
+    echo "none" > "$queue/scheduler"
+    echo "0" > "$queue/iostats"
+    echo "0" > "$queue/add_random"
+    echo "0" > "$queue/nomerges"
+    echo "128" > "$queue/nr_requests"
+    echo "2" > "$queue/rq_affinity"
+    echo "1024" > "$queue/read_ahead_kb"
+done ;
+
+for queue in /sys/block/dm*/queue
+do
+    echo "none" > "$queue/scheduler"
+    echo "0" > "$queue/iostats"
+    echo "0" > "$queue/add_random"
+    echo "0" > "$queue/nomerges"
+    echo "128" > "$queue/nr_requests"
+    echo "2" > "$queue/rq_affinity"
+    echo "1024" > "$queue/read_ahead_kb"
+done ;
+
+for queue in /sys/block/ram*/queue
+do
+    echo "none" > "$queue/scheduler"
+    echo "0" > "$queue/iostats"
+    echo "0" > "$queue/add_random"
+    echo "0" > "$queue/nomerges"
+    echo "128" > "$queue/nr_requests"
+    echo "2" > "$queue/rq_affinity"
+    echo "2048" > "$queue/read_ahead_kb"
+done ;
+
+for queue in /sys/block/zram*/queue
+do
+    echo "none" > "$queue/scheduler"
+    echo "0" > "$queue/iostats"
+    echo "0" > "$queue/add_random"
+    echo "0" > "$queue/nomerges"
+    echo "128" > "$queue/nr_requests"
+    echo "2" > "$queue/rq_affinity"
+    echo "2048" > "$queue/read_ahead_kb"
+done ;
+#
+echo "Done ....."
+exit 0
